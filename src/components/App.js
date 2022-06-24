7879f39e1bd74311e4352aee51aacb12611693f3
DhanushM12 <dhanushmukhi@gmail.com>
2022-06-24 22:14:21 +0530

    added code

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import TodoMain from './TodoMain'
+
+function App() {
+  return (
+    <div>
+    Todo App
+    <TodoMain />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/TodoMain.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Task from './Task'
+
+function TodoMain() {
+  return (
+    <div>
+        <Task />
+    </div>
+  )
+}
+
+export default TodoMain
