0d96dc916715e15f5a027f100a6fed6850f2fd0f
DhanushM12 <dhanushmukhi@gmail.com>
2022-06-27 21:59:01 +0530

    added input box and timer

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 import TodoMain from './TodoMain'
+import '../App.css'
 
 function App() {
+  let time = new Date().toLocaleTimeString();
+  const [currentTime, setCurrentTime] = useState(time);
+  const updateTime = () => {
+    time = new Date().toLocaleTimeString();
+    setCurrentTime(time);
+  }
+  setInterval(updateTime, 1000);
   return (
-    <div>
-    Todo App
+    <div className='App'>
+    <h1>Todo App</h1>
+    <h2>{currentTime}</h2>
     <TodoMain />
     </div>
   )
--- src/components/TodoMain.js
+++ src/components/TodoMain.js
@@ -1,10 +1,22 @@
-import React from 'react'
-import Task from './Task'
+import React, {useState} from 'react'
+import Task from './Task';
+
+// attending session
+
 
 function TodoMain() {
+  // 1 string, 2 - arr
+  const [taskInput, setTaskInput] = useState(""); // input "" -> input - "value" , input -> arr, input -> ""
+  const [taskArray, setTaskArray] = useState([]); // input -> "todo1" -> arr["todo1", "todo2"]
+
+
+  const onTaskChange = (event) => {
+    // console.log(taskInput);
+    setTaskInput(event.target.value)
+  }
   return (
     <div>
-        <Task />
+        <input type="text" value={taskInput} placeholder="Enter a task" onChange={onTaskChange}/>
     </div>
   )
 }
